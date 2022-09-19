fb5be643e234ad6ea79c7808fb5f04825c1f8bd8
Сергей Кривич <krivich@retailcrm.ru>
2022-09-20 00:20:20 +0300

    Add event listener

--- index.php
+++ index.php
@@ -2,21 +2,15 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Simplex\ContentLengthListener;
 use App\Simplex\Framework;
+use App\Simplex\GoogleListener;
+use App\Simplex\ResponseEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-
-function render_template($request): Response
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/src/app.php';
@@ -25,12 +19,16 @@ $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new ContentLengthListener());
+$dispatcher->addSubscriber(new GoogleListener());
+
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
 $path = $request->getPathInfo();
 
-$framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 $response = $framework->handle($request);
 
 $response->send();
