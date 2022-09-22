0040605f9a870eb4fe3e15cc813016b1ab850596
Сергей Кривич <krivich@retailcrm.ru>
2022-09-22 23:06:05 +0300

    Final vers

--- index.php
+++ index.php
@@ -2,39 +2,19 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Simplex\Framework;
 use App\Simplex\StringResponseListener;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 $routes = include __DIR__.'/src/app.php';
+$container = include __DIR__.'/src/container.php';
 
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
+$container->register('listener.string_response', StringResponseListener::class);
+$container->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', [new Reference('listener.string_response')])
+;
 
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new StringResponseListener());
-$listener = new ErrorListener(
-    'App\Calendar\Controller\ErrorController::exception'
-);
-$dispatcher->addSubscriber($listener);
-
-$framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$response = $framework->handle($request);
-$response->send();
+$container->setParameter('charset', 'UTF-8');
+//$container->setParameter('routes', include __DIR__.'/src/app.php');
+$response = $container->get('framework')->handle($request)->send();
